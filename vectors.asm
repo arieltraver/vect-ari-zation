b3302c7c569819382d405c9903c6770467854f13
arieltraver <arieltraver@users.noreply.github.com>
2023-07-13 22:44:31 +0300

    trying to print multiply results rip

--- vectors.asm
+++ vectors.asm
@@ -1,15 +1,20 @@
 section .data
-    msg db "Hello world!", 0ah
-    numbers	db  34,  45,  56,  67,  75, 89
+    msg db "Hello world! result is:", 0ah
 
 section .text
     global _start
 
 _start:
+    mov rdi, 1 ; stdout
+    mov rsi, msg ;the message
+    mov rax, 1 ;syscall for writing
+    mov rdx, 25 ;bytes in the message
+    syscall
+    mov rax, 20
+    mov rcx, 23
+    mul rcx
+    mov rsi, rax
     mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
     syscall
     mov rax, 60
     mov rdi, 0
