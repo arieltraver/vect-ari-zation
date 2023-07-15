53f4c068a9fc1f7b40ed8ddaf69545671b76df6c
arieltraver <arieltraver@users.noreply.github.com>
2023-07-15 18:38:59 +0300

    need to convert decimal into string

--- vectors.asm
+++ vectors.asm
@@ -1,20 +1,22 @@
 section .data
-    msg db "Hello world! result is:", 0ah
+    msg db "Hello world!", 0ah
 
 section .text
     global _start
 
 _start:
-    mov rdi, 1 ; stdout
-    mov rsi, msg ;the message
-    mov rax, 1 ;syscall for writing
-    mov rdx, 25 ;bytes in the message
-    syscall
-    mov rax, 20
-    mov rcx, 23
-    mul rcx
-    mov rsi, rax
+    mov rsi, msg
     mov rax, 1
+    mov rdi, 1
+    mov rdx, 13
+    syscall        ;NOTE: need to convert the decimal into the letter string to print
+    mov rax, 16
+    mov rcx, 8
+    imul rcx, rax
+    lea rsi, [rcx]
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 3
     syscall
     mov rax, 60
     mov rdi, 0
