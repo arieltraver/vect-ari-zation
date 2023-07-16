0ade0187d1d57a6abd406d004a96d10e094229ce
arieltraver <arieltraver@users.noreply.github.com>
2023-07-16 18:04:15 +0300

    need to write helper for 32bit-->ascii (ITOA)

--- vectors.asm
+++ vectors.asm
@@ -1,5 +1,6 @@
 section .data
     msg db "Hello world!", 0ah
+    msg2 db ""
 
 section .text
     global _start
@@ -10,13 +11,16 @@ _start:
     mov rdi, 1
     mov rdx, 13
     syscall        ;NOTE: need to convert the decimal into the letter string to print
-    mov rax, 16
+    mov rbx, 16
     mov rcx, 8
-    imul rcx, rax
-    lea rsi, [rcx]
+    imul rbx, rcx ;stored in bx
+    mov rcx, rbx
+    mov rsi, rcx
+    mov [msg2], rbx
+    mov rsi, msg2
     mov rax, 1
-    mov rdi, 1
-    mov rdx, 3
+    mov rdx, 1
+    mov rdx, 32
     syscall
     mov rax, 60
     mov rdi, 0
