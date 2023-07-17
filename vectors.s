3e7d168989623f2e465584df462c304a3e8d833c
Ubuntu <ubuntu@ip-172-31-19-254.us-east-2.compute.internal>
2023-07-17 23:42:28 +0000

    compiler being annoying

--- vectors.s
+++ vectors.s
@@ -1,11 +1,11 @@
 global _start
 
-_start:
 section .data
-	arr0: dword 1.1, 2.2, 3.3, 4.4
-	arr1: dword 5.5, 6.6, 7.7, 8.8
-	zeros: dword 0, 0, 0, 0
+	arr0 dword 1.1, 2.2, 3.3, 4.4
+	arr1 dword 5.5, 6.6, 7.7, 8.8
+	zeros dword 0, 0, 0, 0
 section .text
+_start:
 	movups xmm0, arr0 ;move first array
 	movups xmm1, arr1
 	mulps xmm0, xmm1
