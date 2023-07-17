4dbd835bffd874f07818188c98eaee79d5142970
Ari <ari@ari>
2023-07-17 23:44:51 +0000

    arrays accepted

--- vectors.s
+++ vectors.s
@@ -1,9 +1,9 @@
 global _start
 
 section .data
-	arr0 dword 1.1, 2.2, 3.3, 4.4
-	arr1 dword 5.5, 6.6, 7.7, 8.8
-	zeros dword 0, 0, 0, 0
+	arr0 dw 1.1, 2.2, 3.3, 4.4
+	arr1 dw 5.5, 6.6, 7.7, 8.8
+	zeros dw 0, 0, 0, 0
 section .text
 _start:
 	movups xmm0, arr0 ;move first array
