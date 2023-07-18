610c72e27b91fb1d07fa88db05baa5fbadbe76df
Ari <ari@ari>
2023-07-18 23:21:07 +0000

    used malloc still segfault also forgot to free

--- dotTest.c
+++ dotTest.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
 	extern void dotProduct(float *arr0, float *arr1, unsigned long int arrlen);
-	float array0[] = {1.1, 2.2, 3.3, 4.4};
-	float array1[] = {2.0, 2.0, 2.0, 2.0};
-	dotProduct(array0, array1, 128);
+	float* array0 = (float*) malloc(8 * sizeof(float));
+	float* array1 = (float*) malloc(8 * sizeof(float));
+	for (int i = 0; i < 8; i++){
+		array0[i] = 1.1 * i;
+		array1[i] = 2.0;
+	}
+	dotProduct(array0, array1, 8);
 	printf("%f, %f, %f, %f\n", array0[0], array0[1], array0[2], array0[3]);
 	printf("%f, %f, %f, %f\n", array1[0], array1[1], array1[2], array1[3]);
 }
