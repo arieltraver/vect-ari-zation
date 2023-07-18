fa65fa0db7402b9cb64f2dc74c33c514c986313b
Ari <ari@ari>
2023-07-18 19:00:03 +0000

    creating loop logic for large arrays etc

--- dotproduct.s
+++ dotproduct.s
@@ -1,21 +1,34 @@
-global _start
+global _dotProduct
 
 section .data
 	arr0 dw 1.1, 2.2, 3.3, 4.4
 	arr1 dw 2.0, 2.0, 2.0, 2.0
 	zeros dw 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-section .text
-_start:
-	vzeroall
-break0:
-	movups xmm0, [arr0] ;move first array
-	movups xmm1, [arr1]
-break1:	mulps xmm0, xmm1
-	movups [arr0], xmm0 ;store result in first array
+section .text ;pointer to arrays in rsi and rdi, length stored in rdx, result stored in the address pointed to by rsi
+_dotProduct: 	pop rbp
+		mov rbp, rsp ;new stack frame
+		vmovaps ymm0, zeros
+		vmovaps ymm1, zeros ;zero out these registers
+
+comp:
+		
+		cmp 256, rdx ;rdx stores remaining len of array
+		jle workPart ;working on a non-full-sized chunk, ending
+		sub rdx, 256
+workChunk:
+		vmovaps ymm0, [rsi] ;move 256 unaligned bytes of first array
+		vmovaps ymm1, [rdi] ;heap address stored in rsi and rdi
+		vmulps ymm0, ymm1 ;dot of this section
+		vmovaps ymm0, [rsi]
+		add rdi, 256 ;next block
+		add rdi, 256
+		jmp =comp
+		
+		movups [arr0], xmm0 ;store result in first array
 break2:
-exit:	mov rax, 60
-	mov rdi, 0
-	syscall 
+exit:		mov rax, 60
+		mov rdi, 0
+		syscall 
 		
 
 
