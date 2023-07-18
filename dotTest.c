5eeb976ff76847114115a3e036be8dd0e8abe3f3
Ari <ari@ari>
2023-07-18 22:05:52 +0000

    fixed parameter disorder

--- dotTest.c
+++ dotTest.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-	extern void dotProduct(unsigned long int arrLen, float *arr0, float *arr2);
+	extern void dotProduct(float *arr0, float *arr1, unsigned long int arrlen);
 	float array0[] = {1.1, 2.2, 3.3, 4.4};
 	float array1[] = {2.0, 2.0, 2.0, 2.0};
-	dotProduct(128, array0, array1);
+	dotProduct(array0, array1, 128);
 }
