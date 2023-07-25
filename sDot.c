27acc67959d0838dbade690d472cbad220fb0c3b
Ari <ari@ari>
2023-07-25 17:52:08 +0000

    more threadstuff

--- sDot.c
+++ sDot.c
@@ -7,18 +7,20 @@ extern float dotProduct(float *arr0, float *arr1);
 struct args {
     float* arr0;
     float* arr1;
+    float* resultIndex;
 };
 
 void *oneChunk(void *input) {
     printf("product: %f", dotProduct( ((struct args*)input)->arr0, ((struct args*)input)->arr1));
 }
 
-
+//note: absolute vals (length etc) will or hypothetically will be replaced by variables, we are just using them for testing purposes.
 int main(int argc, char *argv[]){
 	//you need these arrays aligned with 32 in order to fit into ymme regs
 	float* array0 = (float*) aligned_alloc(32, 16 * sizeof(float));
 	printf("%ld\n", sizeof(float));
 	float* array1 = (float*) aligned_alloc(32, 16 * sizeof(float));
+	float* resultsArr = (float*) malloc(2 * sizeOf(float))
 	for (int i = 0; i < 16; i++){
 		array0[i] = 1.1 * i;
 		array1[i] = 1.0;
@@ -34,9 +36,9 @@ int main(int argc, char *argv[]){
 		struct args *Args1 = (struct args *)malloc(sizeof(struct args));
 		Args1->arr0 = array0 + i;
 		Args1->arr1 = array1 + i;
+		Args1->resultsIndex = resultsArr + i;
 		argsArr[i] = (void *)Args1;
 	}
-
 	pthread_t* chunks = malloc(2 * sizeof(pthread_t))
 	for (int i = 0; i < 2; i++) {
 		pthread_t chunk1;
@@ -49,6 +51,11 @@ int main(int argc, char *argv[]){
 	for (int i=0; i < 2; i++) {
 		pthread_join(chunks[i], NULL);
 	}
+	float sum = 0;
+	for (int i=0; i < 2; i++) {
+		sum += resultsArr[i]
+	}
+	printf("%f\n", sum);
 	/*
 	pthread_t chunk1;
 	pthread_create(&chunk1, NULL, oneChunk, (void *)Args1);
@@ -57,4 +64,5 @@ int main(int argc, char *argv[]){
 	free(argsArr);
 	free(array0);
 	free(array1);
+	free(resultsArr);
 }
